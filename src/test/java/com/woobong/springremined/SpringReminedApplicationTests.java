40d1f40bf09c49a7d2040b76394ac59e5620c6e5
wb.heo <wb.heo@dalkomm.com>
2018-02-27 16:02:35 +0900

    create project

---
+++ src/test/java/com/woobong/springremined/SpringReminedApplicationTests.java
@@ -0,0 +1,16 @@
+package com.woobong.springremined;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringReminedApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
